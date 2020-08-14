23ea9f4dfbbd67f1365979034714af0ca9cf0d68
davawen <56738724+davawen@users.noreply.github.com>
2020-08-15 00:26:20 +0200

    Added p5 and express

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-var fs = require('fs');
-var http = require('http');
-
 const port = 8080;
 
-function handleRequest(req, res)
-{
-    fs.readFile(__dirname + req.url,
-        function(err,data)
-        {
-            if(err) 
-            {
-                res.writeHead(404);
-                res.end(JSON.stringify(err));
-                return;
-            }
-        res.writeHead(200);
-        res.end(data);
-        }
-    );
-}
+var express = require('express');
 
-var server = http.createServer(handleRequest);
-server.listen(port);
+var app = express();
+var server = app.listen(port);
+
+app.use(express.static('public'));
