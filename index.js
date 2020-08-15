a20fa8f93bc96596b88f0b271d81f2cbc64897b5
davawen <56738724+davawen@users.noreply.github.com>
2020-08-15 22:31:30 +0200

    Added synchronous movement

--- index.js
+++ index.js
@@ -6,3 +6,34 @@ var app = express();
 var server = app.listen(port);
 
 app.use(express.static('public'));
+
+let io = require('socket.io').listen(server);
+
+let users = {};
+let numUsers = 0;
+
+io.sockets.on('connection',
+    function(socket)
+    {
+        numUsers++;
+        users[socket.id] = {pos: {x: 400, y:400}};
+        
+        io.emit('newConnection', users);
+        
+        socket.on('position',
+            function(data)
+            {
+                users[socket.id].pos = data;
+                socket.broadcast.emit('position', {'id': socket.id, 'pos': data});
+            }
+        );
+        
+        socket.on('disconnect',
+            function()
+            {
+                delete users[socket.id];
+                numUsers--;
+            }
+        );
+    }
+);
--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,58 @@
+let users = {};
+let socket, socketId;
+
+let position;
+
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(800, 800);
+	
+	socket = io.connect();
+	
+	socket.on('newConnection',
+		function(data)
+		{
+			users = data;
+			
+			socketId = socket.id;
+		}
+	);
+	
+	socket.on('position',
+		function(data)
+		{
+			users[data.id].pos = data.pos;
+		}
+	);
 }
 
 function draw() {
 	background(100);
 	
+	noStroke();
 	fill(255);
-	circle(mouseX, mouseY, 10);
+	
+	//circle(position.x, position.y, 20);
+	for(id in users)
+	{
+		var p = users[id];
+		
+		circle(p.pos.x, p.pos.y, 20);
+	}
+	
+	//#region Logic
+	
+	if(socketId)
+	{
+		var x = keyIsDown(68) - keyIsDown(65);
+		var y = keyIsDown(83) - (keyIsDown(90) || keyIsDown(87));
+		
+		var p = users[socketId];
+		p.pos.x += x*5;
+		p.pos.y += y*5;
+		
+		socket.emit('position', p.pos);
+		
+	}
+	
+	//#endregion
 }
